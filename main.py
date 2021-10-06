b7908349b459306d65ba9721d82b659984985d50
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-06 11:49:39 +0800

    Add import os to clear output

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-from IPython.display import clear_output
+import os
 from random import randint, shuffle
 
 def display_board(board):
-    clear_output()
+  os.system('clear')
 
-    """
-    Prints an empty playing board to the user
-    """
+  """
+  Prints an empty playing board to the user
+  """
     
-    print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
+  print('   |   |')
+  print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+  print('   |   |')
+  print('-----------')
+  print('   |   |')
+  print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+  print('   |   |')
+  print('-----------')
+  print('   |   |')
+  print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+  print('   |   |')
 
 board_positions = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
